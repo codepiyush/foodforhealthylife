5cffb1852293d9f1d4ddf6ac028f9cee568fded5
codepiyush <piyush.bhargav70@gmail.com>
2020-04-09 14:11:10 +0530

    address display added

--- src/sidebar.js
+++ src/sidebar.js
@@ -4,7 +4,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 // import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 class Sidebar extends Component {
-  
+
   render() {
     return (
       <div className="sidebar">
@@ -29,15 +29,15 @@ class Sidebar extends Component {
                 <Link to="/products/cookie#cookie">Cookie</Link>
               </div>
               <div className="smitem" id="sportsd" >
-              <Link to="/products/drinks#drinks">Drinks</Link>
+                <Link to="/products/drinks#drinks">Drinks</Link>
               </div>
               <div className="smitem" >
-              <Link to="/products/healthy#healthy">Healthy Cookies</Link>
+                <Link to="/products/healthy#healthy">Healthy Cookies</Link>
               </div>
               <div className="smitem" >
                 <Link to="/products/others#others">Upcomming Products</Link>
               </div>
-              
+
             </div>
           </div>
           <div className="menuContainer">
@@ -63,10 +63,10 @@ class Sidebar extends Component {
           <div className="menuContainer">
             <div className="sideMenuLink">
               <div>
-                <i class="fas fa-users"></i>
+                <i class="fas fa-map-marked-alt"></i>
               </div>
               <div>
-                <p><Link to="/presence">OurPresence</Link></p>
+                <p><Link to="/presence">Our Presence</Link></p>
               </div>
             </div>
           </div>
